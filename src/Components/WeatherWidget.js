3b931a620e3d362bd132ebb2aa6a3a8256f88389
Sebastian Tran <quangson.tran175@gmail.com>
2022-09-20 23:18:20 +0930

    Added docs

--- src/Components/WeatherWidget.js
+++ src/Components/WeatherWidget.js
@@ -50,7 +50,7 @@ function WeatherWidget() {
 
     return (
         <div className="WeatherWidget">
-            <div>Weather Application</div>
+
 
             <Space direction="vertical" align="start">
                 <Search
@@ -64,19 +64,20 @@ function WeatherWidget() {
             </Space>
 
 
+
             <Row gutter={{xs: 16, sm: 32, md: 48, lg: 64}} align="start">
                 <Col align="start">
                     <div
                         className="City">{location[0].LocalizedName ? <>{location[0].LocalizedName}</> : <>Melbourne</>} </div>
                 </Col>
                 <Col xs={2} sm={4} md={6} lg={8} xl={10} align="start">
-                    <div>{liveData[0] ? <div>{liveData[0].LocalObservationDateTime.substring(0, 10)}</div> :
-                        <div></div>}</div>
-                    <div>{liveData[0] ? <div>{liveData[0].LocalObservationDateTime.substring(11, 16)}</div> :
-                        <div></div>}</div>
-                    <div>{liveData[0] ? <div>Humidity: {liveData[0].RelativeHumidity}</div> : null}</div>
-                    <div>{liveData[0] ? <div>Wind Direction: {liveData[0].Wind.Direction.English}</div> :
-                        <div></div>}</div>
+                    <p>{liveData[0] ? <div>{liveData[0].LocalObservationDateTime.substring(0, 10)}</div> :
+                        <div></div>}</p>
+                    <p>{liveData[0] ? <div>{liveData[0].LocalObservationDateTime.substring(11, 16)}</div> :
+                        <div></div>}</p>
+                    <p>{liveData[0] ? <div>Humidity: {liveData[0].RelativeHumidity}</div> : null}</p>
+                    <p>{liveData[0] ? <div>Wind Direction: {liveData[0].Wind.Direction.English}</div> :
+                        <div></div>}</p>
                 </Col>
             </Row>
 
@@ -86,24 +87,24 @@ function WeatherWidget() {
                     <Col>
                         <Card size="small" bordered={false} title="Today" style={{width: 150}}
                               cover={data.DailyForecasts ?
-                                  <img src={`/icon/${data.DailyForecasts[0].Day.Icon}.svg`}/> : null}>
-                            {data.DailyForecasts ? <div>{data.DailyForecasts[0].Day.IconPhrase}</div> : null}
+                                  <img src={`/icon/${data.DailyForecasts[0].Day.Icon}.svg`}  alt="Weather that day"/> : null}>
+                            {data.DailyForecasts ? <p>{data.DailyForecasts[0].Day.IconPhrase}</p> : null}
                             {data.DailyForecasts ?
-                                <div>High {data.DailyForecasts[0].Temperature.Maximum.Value}°{metricSelection ? <>C</> : <>F</>}</div> : null}
+                                <p>High {data.DailyForecasts[0].Temperature.Maximum.Value}°{metricSelection ? <>C</> : <>F</>}</p> : null}
                             {data.DailyForecasts ?
-                                <div>Low {data.DailyForecasts[0].Temperature.Minimum.Value}°{metricSelection ? <>C</> : <>F</>}</div> : null}
+                                <p>Low {data.DailyForecasts[0].Temperature.Minimum.Value}°{metricSelection ? <>C</> : <>F</>}</p> : null}
                         </Card>
                     </Col>
 
                     <Col>
                         <Card size="small" bordered={false} title="Tomorrow" style={{width: 150}}
                               cover={data.DailyForecasts ?
-                                  <img src={`/icon/${data.DailyForecasts[1].Day.Icon}.svg`}/> : null}>
-                            {data.DailyForecasts ? <div>{data.DailyForecasts[1].Day.IconPhrase}</div> : null}
+                                  <img src={`/icon/${data.DailyForecasts[1].Day.Icon}.svg`}  alt="Weather that day"/> : null}>
+                            {data.DailyForecasts ? <p>{data.DailyForecasts[1].Day.IconPhrase}</p> : null}
                             {data.DailyForecasts ?
-                                <div>High {data.DailyForecasts[1].Temperature.Maximum.Value}°{metricSelection ? <>C</> : <>F</>}</div> : null}
+                                <p>High {data.DailyForecasts[1].Temperature.Maximum.Value}°{metricSelection ? <>C</> : <>F</>}</p> : null}
                             {data.DailyForecasts ?
-                                <div>Low {data.DailyForecasts[1].Temperature.Minimum.Value}°{metricSelection ? <>C</> : <>F</>}</div> : null}
+                                <p>Low {data.DailyForecasts[1].Temperature.Minimum.Value}°{metricSelection ? <>C</> : <>F</>}</p> : null}
                         </Card>
                     </Col>
 
@@ -112,12 +113,12 @@ function WeatherWidget() {
                               title={data.DailyForecasts ? data.DailyForecasts[2].Date.substring(5, 10) : ""}
                               style={{width: 150}}
                               cover={data.DailyForecasts ?
-                                  <img src={`/icon/${data.DailyForecasts[2].Day.Icon}.svg`}/> : null}>
-                            {data.DailyForecasts ? <div>{data.DailyForecasts[2].Day.IconPhrase}</div> : null}
+                                  <img src={`/icon/${data.DailyForecasts[2].Day.Icon}.svg`} alt="Weather that day"/> : null}>
+                            {data.DailyForecasts ? <p>{data.DailyForecasts[2].Day.IconPhrase}</p> : null}
                             {data.DailyForecasts ?
-                                <div>High {data.DailyForecasts[2].Temperature.Maximum.Value}°{metricSelection ? <>C</> : <>F</>}</div> : null}
+                                <p>High {data.DailyForecasts[2].Temperature.Maximum.Value}°{metricSelection ? <>C</> : <>F</>}</p> : null}
                             {data.DailyForecasts ?
-                                <div>Low {data.DailyForecasts[2].Temperature.Minimum.Value}°{metricSelection ? <>C</> : <>F</>}</div> : null}
+                                <p>Low {data.DailyForecasts[2].Temperature.Minimum.Value}°{metricSelection ? <>C</> : <>F</>}</p> : null}
                         </Card>
                     </Col>
 
@@ -126,12 +127,12 @@ function WeatherWidget() {
                               title={data.DailyForecasts ? data.DailyForecasts[3].Date.substring(5, 10) : ""}
                               style={{width: 150}}
                               cover={data.DailyForecasts ?
-                                  <img src={`/icon/${data.DailyForecasts[3].Day.Icon}.svg`}/> : null}>
-                            {data.DailyForecasts ? <div>{data.DailyForecasts[3].Day.IconPhrase}</div> : null}
+                                  <img src={`/icon/${data.DailyForecasts[3].Day.Icon}.svg`} alt="Weather that day"/> : null}>
+                            {data.DailyForecasts ? <p>{data.DailyForecasts[3].Day.IconPhrase}</p> : null}
                             {data.DailyForecasts ?
-                                <div>High {data.DailyForecasts[3].Temperature.Maximum.Value}°{metricSelection ? <>C</> : <>F</>}</div> : null}
+                                <p>High {data.DailyForecasts[3].Temperature.Maximum.Value}°{metricSelection ? <>C</> : <>F</>}</p> : null}
                             {data.DailyForecasts ?
-                                <div>Low {data.DailyForecasts[3].Temperature.Minimum.Value}°{metricSelection ? <>C</> : <>F</>}</div> : null}
+                                <p>Low {data.DailyForecasts[3].Temperature.Minimum.Value}°{metricSelection ? <>C</> : <>F</>}</p> : null}
                         </Card>
                     </Col>
 
@@ -140,12 +141,12 @@ function WeatherWidget() {
                               title={data.DailyForecasts ? data.DailyForecasts[4].Date.substring(5, 10) : ""}
                               style={{width: 150}}
                               cover={data.DailyForecasts ?
-                                  <img src={`/icon/${data.DailyForecasts[4].Day.Icon}.svg`}/> : null}>
-                            {data.DailyForecasts ? <div>{data.DailyForecasts[4].Day.IconPhrase}</div> : null}
+                                  <img src={`/icon/${data.DailyForecasts[4].Day.Icon}.svg`} alt="Weather that day"/> : null}>
+                            {data.DailyForecasts ? <p>{data.DailyForecasts[4].Day.IconPhrase}</p> : null}
                             {data.DailyForecasts ?
-                                <div>High {data.DailyForecasts[4].Temperature.Maximum.Value}°{metricSelection ? <>C</> : <>F</>}</div> : null}
+                                <p>High {data.DailyForecasts[4].Temperature.Maximum.Value}°{metricSelection ? <>C</> : <>F</>}</p> : null}
                             {data.DailyForecasts ?
-                                <div>Low {data.DailyForecasts[4].Temperature.Minimum.Value}°{metricSelection ? <>C</> : <>F</>}</div> : null}
+                                <p>Low {data.DailyForecasts[4].Temperature.Minimum.Value}°{metricSelection ? <>C</> : <>F</>}</p> : null}
                         </Card>
                     </Col>
                 </Row>
---
+++ src/docs/WeatherWidget_API.md
@@ -0,0 +1,9 @@
+## API used in this widget
+
+
+
+| Method |                                   API                                    |          Description          |
+|:------:|:------------------------------------------------------------------------:|:-----------------------------:|
+|  GET   |   http://dataservice.accuweather.com/locations/v1/cities/autocomplete    |  Return Closest Matched City  |
+|  GET   |  http://dataservice.accuweather.com/currentconditions/v1/{locationKey}   | Return Live Weather Condition |
+|  GET   | http://dataservice.accuweather.com/forecasts/v1/daily/5day/{locationKey} |     Return 5-day Forecast     |
