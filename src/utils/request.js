43d43ed712b0e5e670a82160bd81ea58c1736eb2
Sebastian Tran <quangson.tran175@gmail.com>
2022-09-21 21:09:23 +0930

    Done part 1

---
+++ src/utils/request.js
@@ -0,0 +1,22 @@
+// Each keys in this configuration file is good for 50 calls/ day due to the app being built with free AccuWeather API free tier.
+// Please change the API key to other keys if the page is not refreshing
+
+import config from '../config.json'
+import axios from "axios";
+
+const getClosestLocation = (value) =>
+    axios.get(`http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${config.api_key}&q=${value}`)
+
+const get5daysData = (locationID, metricSelection) =>
+    axios.get(`http://dataservice.accuweather.com/forecasts/v1/daily/5day/${locationID}?details=false&apikey=${config.api_key}&metric=${metricSelection}`)
+
+const getLiveData = (locationID, metricSelection) =>
+    axios.get(`http://dataservice.accuweather.com/currentconditions/v1/${locationID}?details=true&apikey=${config.api_key}&metric=${metricSelection}`)
+
+
+
+export {
+    getClosestLocation,
+    get5daysData,
+    getLiveData
+}
