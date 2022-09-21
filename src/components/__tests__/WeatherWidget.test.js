4a343d7b08a9eb6b945e59a2d6c34bc6202d1c75
Sebastian Tran <quangson.tran175@gmail.com>
2022-09-21 14:23:17 +0930

    working version 2

---
+++ src/components/__tests__/WeatherWidget.test.js
@@ -0,0 +1,27 @@
+import {render, screen} from "@testing-library/react";
+import '@testing-library/jest-dom'
+import WeatherWidget from "../WeatherWidget";
+
+
+// Mock Window Element
+Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: jest.fn().mockImplementation(query => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(), // Deprecated
+        removeListener: jest.fn(), // Deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+    })),
+});
+
+
+// Test that the page can be initialised upon render
+test('Basic Initial Render', () => {
+    render(<WeatherWidget />);
+    const linkElement = screen.getByText(/search/i);
+    expect(linkElement).toBeInTheDocument();
+});
