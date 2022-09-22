90eda052d83c97b8f3c3faa489beb5f389ee9466
Sebastian Tran <quangson.tran175@gmail.com>
2022-09-22 13:06:17 +0930

    Testing OK

--- src/WeatherWidgetDemoPage.js
+++ src/WeatherWidgetDemoPage.js
@@ -2,7 +2,13 @@ import WeatherWidget from "./components/WeatherWidget";
 
 function WeatherWidgetDemoPage() {
 return (
-    WeatherWidget(26216,"Melbourne",true)
+    // WeatherWidget(26216,"Melbourne",true)
+
+    <WeatherWidget
+        initialLocationKey={26216}
+        initialLocationName={"Melbourne"}
+        initialMetricSelection={true}
+    />
 )
 }
 
